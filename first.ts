40e37c01463c5d5f617e9ecdab2780f551813335
pung8146 <pung8146@naver.com>
2023-03-03 12:07:47 +0900

    ! , tuple , enum 개념

--- first.ts
+++ first.ts
@@ -1,5 +1,5 @@
 const a: string = "5";
-const aa = '5';
+const aa = '5'; 
 // 이때 굳이 string 을 적을 필요가 없습니다 
 // 타입스크립트가 자동으로 추론 해주기에 틀릴경우에만 직접 수정해주면 됩니다. 
 const b: number = 5;
@@ -33,6 +33,7 @@ const add(x:number , y:number) => number => (x,y) => x + y;
 const add: Add = (x, y) => x + y;
 // 화살표 함수에서에 타입스크립트 표기법 2번
 
+// 타입 스크립트에서 빈배열 never 
 // never 타입 빈배열이 나올땐 타입을 지정해주는것이 좋음
 try {
     const array: string[] = [];
@@ -56,3 +57,45 @@ interface Add {
 }
 
 const add: Add = (x , y) => x + y;
+
+// 타입스크립트의 느낌표#####
+// 별로 권장 되지 않음 
+// null 아님을 확실히 선언 할때 사용 합니다.
+const head = document.querySelector('#head')!;
+
+// 추천 하는 방법
+const head = document.querySelector('#head');
+if(head){
+    head.innerHTML = 'hello'
+}
+
+// 타입을 새롭게 만들 수 있습니다 ###
+// 타입을 정교하게 만들 수 있습니다.
+type World = 'world';
+const a: World = 'world';
+
+const b = `hello ${a}`;
+// type Greeting = 'hello world"
+type Greeting = `hello ${World}`
+
+// rest 에서도 규칙이 적용됩니다 ###
+function rest(...args: string[]) {
+    console.log(args)
+}
+rest('1','2','3')
+
+// ## tuple 튜플 [크기가 고정되어있는 배열]
+const tuple: [string , number] = ['1' , 1]
+tuple[2] = 'hello' // 막아주지만
+tuple.push('hello') // 에러표시가 되지 않습니다.
+
+// ## enum 이넘 
+// 순서대로  0 1 2 3 이 부여됩니다.
+// 여러개의 변수들을 하나의 그룹으로 묶고싶을때 사용합니다.
+const enum EDirection {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+const a = EDirection.up
